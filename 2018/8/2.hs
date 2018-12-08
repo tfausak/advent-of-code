c4588983fc2d0a6b81f46d5c70c2fd8bf89c7486
Taylor Fausak <taylor@fausak.me>
2018-12-08 12:04:55 -0500

    Solve day 8 part 2

--- 2018/8/2.hs
+++ 2018/8/2.hs
@@ -4,14 +4,21 @@ import qualified Control.Monad.Trans.State as State
 
 main
   = print
-  . sum
-  . allMetadata
+  . value
   . State.evalState getNode
   . map read
   . words
   =<< getContents
 
-allMetadata node = metadata node ++ concatMap allMetadata (children node)
+value node = if null $ children node
+  then sum $ metadata node
+  else sum . map (maybe 0 value . at (children node)) $ metadata node
+
+at list index = case list of
+  first : rest
+    | index == 1 -> Just first
+    | index > 1 -> at rest (index - 1)
+  _ -> Nothing
 
 data Node = Node { children :: [ Node ], metadata :: [ Int ] }
 
