b84a526dbd182521cac638eb2d0cc821659f7252
Taylor Fausak <taylor@fausak.me>
2018-12-24 12:03:35 -0600

    Solve day 24 part 2

---
+++ 2018/24/2.hs
@@ -0,0 +1,239 @@
+-- stack --resolver lts-13.0 script
+import qualified Control.Arrow as Arrow
+import qualified Control.Monad as Monad
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
+import qualified Data.Ord as Ord
+import qualified Data.Set as Set
+import qualified Data.Text as Text
+import qualified Debug.Trace as Debug
+import qualified System.Exit as Exit
+import qualified System.IO as IO
+import qualified Text.ParserCombinators.ReadP as Parse
+import qualified Text.Read as Read
+
+main = do
+  initial <- Maybe.fromJust
+    . toState
+    . map (map read . drop 1 . lines . Text.unpack)
+    . Text.splitOn (Text.pack "\n\n")
+    . Text.pack
+    <$> readFile "input.txt"
+  Monad.forM_ [0 ..] $ \ boost -> do
+    IO.hPutStr IO.stderr $ "Trying boost " <> show boost <> " ... "
+    let
+      start = initial
+        { stateArmies = Map.adjust (Map.map $ \ group -> group
+          { groupAttackDamage = groupAttackDamage group + boost
+          }) ArmyImmuneSystem $ stateArmies initial
+        }
+      finish = doCombat start
+      immuneSystem = Map.findWithDefault Map.empty ArmyImmuneSystem $ stateArmies finish
+      infection = Map.findWithDefault Map.empty ArmyInfection $ stateArmies finish
+    if not (Map.null immuneSystem) && not (Map.null infection)
+      then IO.hPutStrLn IO.stderr "Neither army won."
+      else if Map.null immuneSystem
+        then IO.hPutStrLn IO.stderr "Immune system lost."
+        else do
+          IO.hPutStrLn IO.stderr "Immune system won!"
+          print . sum . map groupUnits $ Map.elems immuneSystem
+          Exit.exitSuccess
+
+doCombat state =
+  if combatIsOver state
+    then state
+    else let newState = fight state in if newState == state
+      then newState
+      else doCombat newState
+
+combatIsOver = any Map.null . stateArmies
+
+fight state = doAttacks state $ findTargets state
+
+doAttacks state
+  = foldr doAttack state
+  . List.sortOn (groupInitiative . snd)
+  . Map.toList
+
+doAttack (d, a) state =
+  case lookupGroup a state of
+    Nothing -> state
+    Just attacker -> case lookupGroup d state of
+      Nothing -> state
+      Just defender -> let
+        damage = attackDamage attacker defender
+        killed = quot damage $ groupHitPoints defender
+        newDefender = defender { groupUnits = groupUnits defender - killed }
+        update = if groupUnits newDefender < 1 then
+            Map.delete $ groupNumber newDefender
+          else
+            Map.insert (groupNumber newDefender) newDefender
+        in state { stateArmies = Map.adjust update (groupArmy newDefender) $ stateArmies state }
+
+lookupGroup group
+  = Map.lookup (groupNumber group)
+  . Map.findWithDefault Map.empty (groupArmy group)
+  . stateArmies
+
+findTargets state
+  = foldr (insertTarget state) Map.empty
+  . List.sortOn sortTargets
+  $ stateGroups state
+
+sortTargets = effectivePower Arrow.&&& groupInitiative
+
+effectivePower group = groupUnits group * groupAttackDamage group
+
+insertTarget state attacker targets =
+  case findTarget attacker targets state of
+    Nothing -> targets
+    Just defender -> Map.insert defender attacker targets
+
+findTarget attacker targets
+  = Maybe.listToMaybe
+  . List.sortOn (sortTarget attacker)
+  . filter (keepTarget targets attacker)
+  . Map.elems
+  . Map.findWithDefault Map.empty (otherArmy $ groupArmy attacker)
+  . stateArmies
+
+keepTarget targets attacker defender
+  = damageModifier attacker defender /= Just ModifierImmune
+  && not (Map.member defender targets)
+
+sortTarget attacker
+  = Ord.Down
+  . (attackDamage attacker Arrow.&&& effectivePower Arrow.&&& groupInitiative)
+
+attackDamage attacker defender = case damageModifier attacker defender of
+  Just ModifierImmune -> 0
+  Nothing -> effectivePower attacker
+  Just ModifierWeak -> 2 * effectivePower attacker
+
+damageModifier attacker defender =
+  if Set.member (groupAttackType attacker) $ groupImmunities defender then
+    Just ModifierImmune
+  else if Set.member (groupAttackType attacker) $ groupWeaknesses defender then
+    Just ModifierWeak
+  else
+    Nothing
+
+otherArmy army = case army of
+  ArmyInfection -> ArmyImmuneSystem
+  ArmyImmuneSystem -> ArmyInfection
+
+stateGroups = concatMap Map.elems . Map.elems . stateArmies
+
+newtype State = State
+  { stateArmies :: Map.Map Army (Map.Map Int Group)
+  } deriving (Eq, Ord, Show)
+
+toState = fmap (State . foldr insertGroup Map.empty) . toGroups
+
+insertGroup group groups = Map.insertWith
+  Map.union (groupArmy group) (Map.singleton (groupNumber group) group) groups
+
+toGroups armies = case armies of
+  [immuneSystem, infection] -> Just $ mappend
+    (map (toGroup ArmyImmuneSystem) $ zip [1 ..] immuneSystem)
+    (map (toGroup ArmyInfection) $ zip [1 ..] infection)
+  _ -> Nothing
+
+data Group = Group
+  { groupArmy :: Army
+  , groupNumber :: Int
+  , groupUnits :: Int
+  , groupHitPoints :: Int
+  , groupWeaknesses :: Set.Set AttackType
+  , groupImmunities :: Set.Set AttackType
+  , groupAttackDamage :: Int
+  , groupAttackType :: AttackType
+  , groupInitiative :: Int
+  } deriving (Eq, Ord, Show)
+
+toGroup army (number, input) = Group
+  { groupArmy = army
+  , groupNumber = number
+  , groupUnits = inputUnits input
+  , groupHitPoints = inputHitPoints input
+  , groupWeaknesses = getSpecialsWith ModifierWeak input
+  , groupImmunities = getSpecialsWith ModifierImmune input
+  , groupAttackDamage = inputAttackDamage input
+  , groupAttackType = inputAttackType input
+  , groupInitiative = inputInitiative input
+  }
+
+getSpecialsWith modifier
+  = Set.fromList
+  . concatMap specialAttackTypes
+  . filter ((== modifier) . specialModifier)
+  . inputSpecials
+
+data Army
+  = ArmyInfection
+  | ArmyImmuneSystem
+  deriving (Eq, Ord, Show)
+
+data Input = Input
+  { inputUnits :: Int
+  , inputHitPoints :: Int
+  , inputSpecials :: [Special]
+  , inputAttackDamage :: Int
+  , inputAttackType :: AttackType
+  , inputInitiative :: Int
+  } deriving (Eq, Show)
+
+instance Read Input where
+  readsPrec _ = Parse.readP_to_S parseInput
+
+parseInput = Input
+  <$> parseInt
+  <*> (Parse.string " units each with " *> parseInt)
+  <*> (Parse.string " hit points" *> Parse.option [] parseSpecials)
+  <*> (Parse.string " with an attack that does " *> parseInt)
+  <*> (Parse.char ' ' *> parseAttackType)
+  <*> (Parse.string " damage at initiative " *> parseInt)
+
+parseSpecials
+  = Parse.string " ("
+  *> Parse.sepBy1 parseSpecial (Parse.string "; ")
+  <* Parse.char ')'
+
+data Special = Special
+  { specialModifier :: Modifier
+  , specialAttackTypes :: [AttackType]
+  } deriving (Eq, Ord, Show)
+
+parseSpecial = Special
+  <$> parseModifier
+  <*> (Parse.string " to " *> Parse.sepBy1 parseAttackType (Parse.string ", "))
+
+data Modifier
+  = ModifierImmune
+  | ModifierWeak
+  deriving (Eq, Ord, Show)
+
+parseModifier = Parse.choice
+  [ ModifierImmune <$ Parse.string "immune"
+  , ModifierWeak <$ Parse.string "weak"
+  ]
+
+data AttackType
+  = AttackTypeBludgeoning
+  | AttackTypeCold
+  | AttackTypeFire
+  | AttackTypeRadiation
+  | AttackTypeSlashing
+  deriving (Eq, Ord, Show)
+
+parseAttackType = Parse.choice
+  [ AttackTypeBludgeoning <$ Parse.string "bludgeoning"
+  , AttackTypeCold <$ Parse.string "cold"
+  , AttackTypeFire <$ Parse.string "fire"
+  , AttackTypeRadiation <$ Parse.string "radiation"
+  , AttackTypeSlashing <$ Parse.string "slashing"
+  ]
+
+parseInt = either fail pure . Read.readEither =<< Parse.munch1 Char.isDigit
--- 2018/24/README.txt
+++ 2018/24/README.txt
@@ -160,3 +160,148 @@ Group 2 contains 4434 units
 In the example above, the winning army ends up with 782 + 4434 = 5216 units.
 
 You scan the reindeer's condition (your puzzle input); the white-bearded man looks nervous. As it stands now, how many units would the winning army have?
+
+--- Part Two ---
+
+Things aren't looking good for the reindeer. The man asks whether more milk and cookies would help you think.
+
+If only you could give the reindeer's immune system a boost, you might be able to change the outcome of the combat.
+
+A boost is an integer increase in immune system units' attack damage. For example, if you were to boost the above example's immune system's units by 1570, the armies would instead look like this:
+
+Immune System:
+17 units each with 5390 hit points (weak to radiation, bludgeoning) with
+ an attack that does 6077 fire damage at initiative 2
+989 units each with 1274 hit points (immune to fire; weak to bludgeoning,
+ slashing) with an attack that does 1595 slashing damage at initiative 3
+
+Infection:
+801 units each with 4706 hit points (weak to radiation) with an attack
+ that does 116 bludgeoning damage at initiative 1
+4485 units each with 2961 hit points (immune to radiation; weak to fire,
+ cold) with an attack that does 12 slashing damage at initiative 4
+
+With this boost, the combat proceeds differently:
+
+Immune System:
+Group 2 contains 989 units
+Group 1 contains 17 units
+Infection:
+Group 1 contains 801 units
+Group 2 contains 4485 units
+
+Infection group 1 would deal defending group 2 185832 damage
+Infection group 1 would deal defending group 1 185832 damage
+Infection group 2 would deal defending group 1 53820 damage
+Immune System group 2 would deal defending group 1 1577455 damage
+Immune System group 2 would deal defending group 2 1577455 damage
+Immune System group 1 would deal defending group 2 206618 damage
+
+Infection group 2 attacks defending group 1, killing 9 units
+Immune System group 2 attacks defending group 1, killing 335 units
+Immune System group 1 attacks defending group 2, killing 32 units
+Infection group 1 attacks defending group 2, killing 84 units
+
+Immune System:
+Group 2 contains 905 units
+Group 1 contains 8 units
+Infection:
+Group 1 contains 466 units
+Group 2 contains 4453 units
+
+Infection group 1 would deal defending group 2 108112 damage
+Infection group 1 would deal defending group 1 108112 damage
+Infection group 2 would deal defending group 1 53436 damage
+Immune System group 2 would deal defending group 1 1443475 damage
+Immune System group 2 would deal defending group 2 1443475 damage
+Immune System group 1 would deal defending group 2 97232 damage
+
+Infection group 2 attacks defending group 1, killing 8 units
+Immune System group 2 attacks defending group 1, killing 306 units
+Infection group 1 attacks defending group 2, killing 29 units
+
+Immune System:
+Group 2 contains 876 units
+Infection:
+Group 2 contains 4453 units
+Group 1 contains 160 units
+
+Infection group 2 would deal defending group 2 106872 damage
+Immune System group 2 would deal defending group 2 1397220 damage
+Immune System group 2 would deal defending group 1 1397220 damage
+
+Infection group 2 attacks defending group 2, killing 83 units
+Immune System group 2 attacks defending group 2, killing 427 units
+
+After a few fights...
+
+Immune System:
+Group 2 contains 64 units
+Infection:
+Group 2 contains 214 units
+Group 1 contains 19 units
+
+Infection group 2 would deal defending group 2 5136 damage
+Immune System group 2 would deal defending group 2 102080 damage
+Immune System group 2 would deal defending group 1 102080 damage
+
+Infection group 2 attacks defending group 2, killing 4 units
+Immune System group 2 attacks defending group 2, killing 32 units
+
+Immune System:
+Group 2 contains 60 units
+Infection:
+Group 1 contains 19 units
+Group 2 contains 182 units
+
+Infection group 1 would deal defending group 2 4408 damage
+Immune System group 2 would deal defending group 1 95700 damage
+Immune System group 2 would deal defending group 2 95700 damage
+
+Immune System group 2 attacks defending group 1, killing 19 units
+
+Immune System:
+Group 2 contains 60 units
+Infection:
+Group 2 contains 182 units
+
+Infection group 2 would deal defending group 2 4368 damage
+Immune System group 2 would deal defending group 2 95700 damage
+
+Infection group 2 attacks defending group 2, killing 3 units
+Immune System group 2 attacks defending group 2, killing 30 units
+
+After a few more fights...
+
+Immune System:
+Group 2 contains 51 units
+Infection:
+Group 2 contains 40 units
+
+Infection group 2 would deal defending group 2 960 damage
+Immune System group 2 would deal defending group 2 81345 damage
+
+Infection group 2 attacks defending group 2, killing 0 units
+Immune System group 2 attacks defending group 2, killing 27 units
+
+Immune System:
+Group 2 contains 51 units
+Infection:
+Group 2 contains 13 units
+
+Infection group 2 would deal defending group 2 312 damage
+Immune System group 2 would deal defending group 2 81345 damage
+
+Infection group 2 attacks defending group 2, killing 0 units
+Immune System group 2 attacks defending group 2, killing 13 units
+
+Immune System:
+Group 2 contains 51 units
+Infection:
+No groups remain.
+
+This boost would allow the immune system's armies to win! It would be left with 51 units.
+
+You don't even know how you could boost the reindeer's immune system or what effect it might have, so you need to be cautious and find the smallest boost that would allow the immune system to win.
+
+How many units does the immune system have left after getting the smallest boost it needs to win?
